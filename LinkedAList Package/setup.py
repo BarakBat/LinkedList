4aba614476062a19d66fa2aca2a0fca752e5a3d8
BarakBat <barakbattach@gmail.com>
2016-10-30 18:32:11 +0200

    Add files via upload

---
+++ LinkedAList Package/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'AllAboutLinkedList',
+  packages = ['AllAboutLinkedList'], # this must be the same as the name above
+  version = '0.1',
+  description = 'implemntation and helpful algorithems for linked lists',
+  author = 'barak battac',
+  author_email = 'barakbattacn@gmail.com',
+  url = 'https://github.com/barakbattach/LinkedList', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/LinkedList/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
